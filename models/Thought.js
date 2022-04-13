effaa5814d7516497034cf51e7a9056a9268b6d5
CodeNinja <inicholas8686@gmail.com>
2022-04-13 13:51:07 -0700

    possible completed models

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+const reactionSchema = require('./Reaction');
+
+// schema to create the Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, 'Thought text is required'],
+      length: [1, 280, 'Thought text must be between 1 and 280 characters'],
+    },
+    createdAt: {
+      type: Date, 
+      default: Date.now,
+      getter: function() {
+        return this.createdAt.toLocaleString();
+      },
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
+    reactions: [{ type: Schema.Types.ObjectId, ref: 'Reaction' }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+
+  // virtual property to get the amount of reactions a thought has
+  thoughtsSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+  // initializes the Thought model
+  const Thought = model('Thought', thoughtSchema);
+
+  module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+const reactionSchema = require('./Reaction');
+
+// schema to create the User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, 'Username is required'],
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address'],
+      required: [true, 'Email is required'],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// virtual property to get the amount of friends a user has
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// initializes the User model
+const User = model('User', userSchema);
+
+module.exports = User;
